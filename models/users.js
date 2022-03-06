c12b9a4399a0d5557301fcbd6c3709883c05c87a
Andrew Febre <afebre1027@gmail.com>
2022-03-06 15:26:07 -0500

    user routes and model

--- models/users.js
+++ models/users.js
@@ -1,16 +1,22 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
-    userName: { 
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    email:{
-        type: String,
-        reuired: true,
-        unique: true,
-        validate: {}
-    },
-})
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    reuired: true,
+    unique: true,
+    match: [/.+@.+\..+/],
+  },
+  thoughts: [{}],
+  friends: [{}],
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
